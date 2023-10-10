0e7bfcde52ba4fc1721467a0d827b422644a5e75
Todd H. Albert <todd@bocacode.com>
2023-10-10 16:58:27 -0400

    connected to postgres from node

---
+++ index.js
@@ -0,0 +1,22 @@
+// import tools from pg library
+import pg from 'pg'
+const { Client } = pg
+
+// import connection string from credentials / secrets file
+import { connectionURI } from './creds.js'
+
+// create client connection to database
+const client = new Client({
+  connectionString: connectionURI,
+})
+
+// open connection
+await client.connect()
+
+// run query
+const results = await client.query('SELECT * FROM customers')
+
+console.table(results.rows)
+
+// close connection
+client.end()
